85020d6be835a0bfe012adb815d19ceada1074eb
itsarunkumar <003arunkumark@gmail.com>
2021-11-06 10:58:13 +0000

    1 file modified, 2 added, 2 deleted

--- src/comp/Expenses/Expenses.js
+++ src/comp/Expenses/Expenses.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React , {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpensesFilter from '../ExpenseFilter/ExpenseFilter';
+import ExpensesFilter from './ExpenseFilter';
 
 function Expenses({ items }) {
+  const [filterdText , setFilteredText] = useState('2021')
+  const filterChangeHandler =(selectedYear)=>{
+    console.log(`---in Expenses js file ${selectedYear}`)
+    setFilteredText(selectedYear)
+  }
   return (
     <div>
     <Card className="expenses">
-      <ExpensesFilter/>
+      <ExpensesFilter filterd={filterdText} onFilter={filterChangeHandler}/>
       <ExpenseItem
         date={items[0].date}
         title={items[0].title}
